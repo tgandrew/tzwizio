15960d3343956b89365319112660985304545ad0
Tom Andrew <tandrew@nvidia.com>
2023-11-02 10:27:34 -0700

    Test commit

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,58 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import * as moment from 'moment-timezone';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+const welcome = `<!DOCTYPE html>
+<html>
+  <meta charset="UTF-8" />
+  <body>
+    <h1>Welcome to ⏳tzwiz.io⏳!</h1>
+    <p>This website gives you a simple way to get Open Graph meta data dynamically to help convert timezones within Slack messages</p>
+    <p>Example message: <code>I'll be out for an appointment at tzwiz.io/pm/est/3</code></p>
+  </body>
+</html>`;
+
+// function getTzfromOffset(offset: string): string {
+// 	var timezones = moment.tz.names();
+
+// 	// Filter the timezones based on the UTC offset
+// 	var offset = Number(offset); // UTC offset in hours
+// 	var matchingTimezones = timezones.filter((name) => {
+// 		var timezoneOffset = moment.tz(name).utcOffset() / 60;
+// 		return timezoneOffset === offset;
+// 	});
+// }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request): Promise<Response> {
+		const url = new URL(request.url);
+
+		if (url.pathname === '/') {
+			return new Response(welcome, { headers: { 'content-type': 'text/html' } });
+		} else {
+			const time = url.pathname.substring(1, 5);
+			const offset = url.pathname.substring(5);
+
+			// if (!ip) {
+			// 	return new Response('No IP address found', { status: 400 });
+			// } else {
+			// 	const [minute, hour] = url.pathname.split('/', 2);
+			// 	const yourTime = moment.tz({ hours: hour, minutes: minute }, timezone);
+			// 	const utcTime = yourTime.utc().format('hh:mm a');
+			// 	const ptTime = yourTime.tz('America/Los_Angeles').format('hh:mm a');
+			// 	const etTime = yourTime.tz('America/New_York').format('hh:mm a');
+
+			// 	const timeInfo = `Pacific ${ptTime} -8/-7.\nEastern ${etTime} -6/-5.\nUTC ${utcTime} UTC +0`;
+			const timeInfo = `timezone ${request.headers.get('X-Forwarded-For')}`;
+			const html = `<!DOCTYPE html>
+        <html>
+          <head>
+            <meta property="og:description" content="${timeInfo}" />
+          </head>
+          <body>
+            <h1>${timeInfo}</h1>
+          </body>
+        </html>`;
+
+			return new Response(html, { headers: { 'content-type': 'text/html' } });
+		}
 	},
 };
